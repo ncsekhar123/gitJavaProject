9430f9805b3a43fd18feac8dafa08ef5fb056fc1
chandra <nchandras123@gmail.com>
2023-08-29 07:19:16 +0530

    Second Commit from Command Bash

--- gitJavaProject/src/welcome.java
+++ gitJavaProject/src/welcome.java
@@ -5,6 +5,7 @@ public class welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Commit from bash ");
+		System.out.println("2nd Commit from bash ");
 	}
 
 }
